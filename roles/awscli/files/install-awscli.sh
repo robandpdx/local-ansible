f12e7050c766f5eff283a6216da37f5bd8484c14
Rob Anderson <robandpdx@github.com>
2022-05-16 16:15:46 -0600

    role for installing awscli

---
+++ roles/awscli/files/install-awscli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg ./AWSCLIV2.pkg -target /
+rm ./AWSCLIV2.pkg
+
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+unzip sessionmanager-bundle.zip
+sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+rm ./sessionmanager-bundle.zip
+rm -rf ./sessionmanager-bundle
